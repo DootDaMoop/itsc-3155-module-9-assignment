93b449c137fd80d021c16a20e1e0e575923880d5
ConnorJAyscue <connorjayscue@gmail.com>
2024-03-19 20:10:12 -0400

    Updating tests

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -11,6 +11,7 @@ def test_get_all_movies():
     movie1 = movie_repo.get_movie_by_title('Star Wars')
     movie2 = movie_repo.get_movie_by_title('Harry Potter')
     movie3 = movie_repo.get_movie_by_title('Huh?')
+    
     movie_dict = movie_repo.get_all_movies()
     
     if movie1 and movie2 and movie3 in movie_dict:
